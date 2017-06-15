bfc8db55b9c6b135df2f74a9d07ccd06e86464df
Heather D'Souza <heather.dsouza@hotmail.com>
2017-06-15 15:46:04 -0400

    fixes dull colours and makes them more visible -- needs to be debugged

--- functions/overlay_trace_colours_on_imgs.m
+++ functions/overlay_trace_colours_on_imgs.m
@@ -10,8 +10,6 @@ function cyto_overlay = overlay_trace_colours_on_imgs(CellsTable, imgs)
         NumberOfCells = size(boundaries_nuc, 1);
         point_in_time = zeros(size(imgs,1), size(imgs,2), 3);
 
-        
-            
             for i=1:NumberOfCells
 
               Object = ObjectsInFrame(i,:);
@@ -26,6 +24,17 @@ function cyto_overlay = overlay_trace_colours_on_imgs(CellsTable, imgs)
               point_in_time(boundaries_nuc{i}) = red;
               point_in_time(boundaries_nuc{i}+size(imgs,1)*size(imgs,2)) = green;
               point_in_time(boundaries_nuc{i}+size(imgs,1)*size(imgs,2)*2) = blue;
+              
+              %check for dull colour
+              hsv = rgb2hsv(point_in_time);
+              luminance = hsv(:,:,3);
+              boundaries = boundaries_nuc{i};
+              if luminance(boundaries(1)) > 0 & luminance(boundaries(1)) < 77
+                  i
+                  luminance(boundaries_nuc{i}) = 77;
+                  hsv(:,:,3) = luminance;
+                  point_in_time = hsv2rgb(hsv);
+              end
 
             end
             
@@ -36,7 +45,7 @@ function cyto_overlay = overlay_trace_colours_on_imgs(CellsTable, imgs)
             
             for i=1:NumberOfCells
                 
-                Object = ObjectsInFrame(i,:);
+              Object = ObjectsInFrame(i,:);
               trace = Object.Trace{:};
               trace = strsplit(trace,'-');
 
@@ -48,15 +57,28 @@ function cyto_overlay = overlay_trace_colours_on_imgs(CellsTable, imgs)
               point_in_time(boundaries_cyto{i}) = red;
               point_in_time(boundaries_cyto{i}+size(imgs,1)*size(imgs,2)) = green;
               point_in_time(boundaries_cyto{i}+size(imgs,1)*size(imgs,2)*2) = blue;
+              
+              %check for dull colour
+              hsv = rgb2hsv(point_in_time);
+              luminance = hsv(:,:,3);
+               boundaries = boundaries_cyto{i};
+              if luminance(boundaries(1)) > 0 & luminance(boundaries(1)) < 77
+                  true
+                  luminance(boundaries_cyto{i}) = 77;
+                  hsv(:,:,3) = luminance;
+                  point_in_time = hsv2rgb(hsv);
+              end
+              
               point_in_time = uint8(point_in_time);
-              imdilate(point_in_time, strel('disk',1));
-              imdilate(point_in_time, strel('disk',1));
-              imdilate(point_in_time, strel('disk',1));
+              
+%               imdilate(point_in_time, strel('disk',1));
+%               imdilate(point_in_time, strel('disk',1));
+%               imdilate(point_in_time, strel('disk',1));
             end
         
 
         labelled_by_trace_id_this(:,:,t,:) = point_in_time;
-
+        
     end
   
     %to view nuc on nuc overlay pass in nuc images instead of cyto
@@ -65,21 +87,7 @@ function cyto_overlay = overlay_trace_colours_on_imgs(CellsTable, imgs)
                  + uint8(cyto_rgb./12);           % add original cyto image
     
     figure; imshow3D(cyto_overlay, [])
+    figure; imshow3D(uint8(labelled_by_trace_id_this./2), []);
   
 end 
  
-%       %% 1) RGB segmentation overlay BY SIZE (useful for seeing size)
-%     boundries_rgb = zeros(size(cyto, 1), size(cyto, 2), size(cyto, 3), 3);
-%     %for i = 1:size(cyto, 3)
-%     for t=min(CellsTable.Time):max(CellsTable.Time)
-%       labelled_by_size_color_fix=labelled_by_size(:,:,i);
-%       labelled_by_size_color_fix(1)=min(ResultsTable.CellSize);
-%       labelled_by_size_color_fix(2)=COLOR_LIMIT;
-%       boundries_rgb(:,:,i,:) = label2rgb(round(labelled_by_size_color_fix),'jet', 'k');
-%     end
-%     mod(sum(uint8('19oueiueoif')), 255); % calculate a colour value between 0 and 255
-%     cyto_rgb = cat(4, cyto, cyto, cyto);
-%     cyto_overlay = uint8(boundries_rgb./6) ... % segmented boundries
-%                  + uint8(cyto_rgb);           % original cyto
-%     % figure('name','cyto_overlay', 'NumberTitle','off');imshow3Dfull(uint8(cyto_overlay),[]);
-% end
