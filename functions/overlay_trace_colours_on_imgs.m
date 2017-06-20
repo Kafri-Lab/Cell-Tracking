8303d74f0ef0a4f899720291cff9739660a00224
Heather D'Souza <heather.dsouza@hotmail.com>
2017-06-20 11:16:06 -0400

    decreased runtime of overlay_trace_colours_on_imgs.m and added comments

--- functions/overlay_trace_colours_on_imgs.m
+++ functions/overlay_trace_colours_on_imgs.m
@@ -3,46 +3,14 @@ function cyto_overlay = overlay_trace_colours_on_imgs(CellsTable, imgs)
     labelled_by_trace_id_this = zeros(size(imgs,1), size(imgs,2), size(imgs,3), 3);
    
     for t=min(CellsTable.Time):max(CellsTable.Time)
-
+        
         ObjectsInFrame = CellsTable(CellsTable.Time==t,:); %ResultTable for cells in frame
         boundaries_nuc = ObjectsInFrame.nuc_boundaries; %nuclei boundaries
         boundaries_cyto = ObjectsInFrame.cyto_boundaries; %cytoplasm boundaries
         NumberOfCells = size(boundaries_nuc, 1);
         point_in_time = zeros(size(imgs,1), size(imgs,2), 3);
 
-            for i=1:NumberOfCells
-
-              Object = ObjectsInFrame(i,:);
-              trace = Object.Trace{:};
-              trace = strsplit(trace,'-');
-
-              %calculate RGB values
-              red = mod(sum(uint8(trace{1})),255);
-              green = mod(sum(uint8(trace{2})),255);
-              blue = mod(sum(uint8(trace{3})),255);
-
-              point_in_time(boundaries_nuc{i}) = red;
-              point_in_time(boundaries_nuc{i}+size(imgs,1)*size(imgs,2)) = green;
-              point_in_time(boundaries_nuc{i}+size(imgs,1)*size(imgs,2)*2) = blue;
-              
-              %check for dull colour
-              hsv = rgb2hsv(point_in_time);
-              luminance = hsv(:,:,3);
-              boundaries = boundaries_nuc{i};
-              if luminance(boundaries(1)) > 0 && luminance(boundaries(1)) < 77
-                  luminance(boundaries_nuc{i}) = 150;
-                  hsv(:,:,3) = luminance;
-                  point_in_time = hsv2rgb(hsv);
-              end
-
-            end
-            
-            %fill in perimeter
-              point_in_time(:,:,1) = imfill(point_in_time(:,:,1),'holes');
-              point_in_time(:,:,2) = imfill(point_in_time(:,:,2),'holes');
-              point_in_time(:,:,3) = imfill(point_in_time(:,:,3),'holes');
-            
-              
+            %colour nucleus
             for i=1:NumberOfCells
                 
               Object = ObjectsInFrame(i,:);
@@ -54,27 +22,65 @@ function cyto_overlay = overlay_trace_colours_on_imgs(CellsTable, imgs)
               green = mod(sum(uint8(trace{2})),255);
               blue = mod(sum(uint8(trace{3})),255);
 
+              %apply colours to nucleus
+              point_in_time(boundaries_nuc{i}) = red;
+              point_in_time(boundaries_nuc{i}+size(imgs,1)*size(imgs,2)) = green;
+              point_in_time(boundaries_nuc{i}+size(imgs,1)*size(imgs,2)*2) = blue;
+
+            end
+            
+            %checks for dulls colours on nucleus
+            hsv = rgb2hsv(point_in_time);
+            luminance = hsv(:,:,3);
+            for i=1:NumberOfCells
+                
+                boundaries = boundaries_nuc{i};
+                if luminance(boundaries(1)) > 0 && luminance(boundaries(1)) < 77
+                    luminance(boundaries_nuc{i}) = 150;
+                end
+                
+            end 
+            hsv(:,:,3) = luminance;
+            point_in_time = hsv2rgb(hsv);
+            
+            %fill in perimeter of nucleus
+            point_in_time(:,:,1) = imfill(point_in_time(:,:,1),'holes');
+            point_in_time(:,:,2) = imfill(point_in_time(:,:,2),'holes');
+            point_in_time(:,:,3) = imfill(point_in_time(:,:,3),'holes');
+            
+            %colour cyto
+            for i=1:NumberOfCells
+                
+              Object = ObjectsInFrame(i,:);
+              trace = Object.Trace{:};
+              trace = strsplit(trace,'-');
+
+              %calculate RGB values
+              red = mod(sum(uint8(trace{1})),255);
+              green = mod(sum(uint8(trace{2})),255);
+              blue = mod(sum(uint8(trace{3})),255);
+
+              %apply colours to cyto
               point_in_time(boundaries_cyto{i}) = red;
               point_in_time(boundaries_cyto{i}+size(imgs,1)*size(imgs,2)) = green;
               point_in_time(boundaries_cyto{i}+size(imgs,1)*size(imgs,2)*2) = blue;
               
-              %check for dull colour
-              hsv = rgb2hsv(point_in_time);
-              luminance = hsv(:,:,3);
-               boundaries = boundaries_cyto{i};
-              if luminance(boundaries(1)) > 0 & luminance(boundaries(1)) < 77
-                  luminance(boundaries_cyto{i}) = 77;
-                  hsv(:,:,3) = luminance;
-                  point_in_time = hsv2rgb(hsv);
-              end
-                          
             end
         
-        
-        point_in_time = uint8(point_in_time);
-        labelled_by_trace_id_this(:,:,t,:) = point_in_time;
-        
-        
+            %check for dull colours on cyto
+            hsv = rgb2hsv(point_in_time);
+            luminance = hsv(:,:,3);
+            for i=1:NumberOfCells
+                boundaries = boundaries_cyto{i};
+                if luminance(boundaries(1)) > 0 && luminance(boundaries(1)) < 77
+                   luminance(boundaries_cyto{i}) = 150;
+                end
+            end 
+            hsv(:,:,3) = luminance;
+            point_in_time = hsv2rgb(hsv);
+            
+            point_in_time = uint8(point_in_time);
+            labelled_by_trace_id_this(:,:,t,:) = point_in_time;
     end
   
     cyto_rgb = cat(4, imgs, imgs, imgs); 
@@ -83,6 +89,5 @@ function cyto_overlay = overlay_trace_colours_on_imgs(CellsTable, imgs)
     
     figure; imshow3D(cyto_overlay, [])
    
-  
 end 
  
