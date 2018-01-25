a52f160b73e7bfd99cf6ce719da31f257a4b5670
Daniel Snider <danielsnider12@gmail.com>
2018-01-24 19:57:05 -0500

    Improvements loop
    Improve DiffMeasurements - Gracefully handle missing measurements
    
    Improve DiffMeasurements - Handle more measurements using loop
    
    multi-channel

--- functions/DifferentialMeasurements.m
+++ functions/DifferentialMeasurements.m
@@ -31,92 +31,90 @@ function [raw_differences, normalized_differences, composite_differences] = Diff
   %       /   number of cells at T1       /   number of cells at T3
   %    number of cells at T2           number of cells at T4
 
-  %% RAW DIFFERENCES
-  raw_differences = {};
+  %% SET METRIC WEIGHTS
+  % Importance of each metric for when calculating composite distances.
+  % Higher value is more important.
+  % Metrics that don't have a weight setting will be ignored.
 
-  count = 1; % this is needed because "t" in the loop may not start at 1, we could
-  % if we really wanted to start processing timepoints in the middle of the sequence
+  % Original version metrics
+  weights = {}; weights.Translation = 5; weights.Eccentricity = 1; weights.Area = 1; weights.MajorAxisLength = 1; weights.MinorAxisLength = 1; weights.Solidity = 1; weights.Orientation = 1; weights.Nuc_intensity = 2;
+
+  % All available metrics
+  weights = {}; weights.C_Area = 1; weights.C_BoundingBox = 1; weights.C_Centroid = 1; weights.C_Eccentricity = 1; weights.C_Solidity = 1; weights.C_MajorAxisLength = 1; weights.C_MinorAxisLength = 1; weights.C_Orientation = 1; weights.C_Ch1_TotalIntensity = 1; weights.C_Ch2_TotalIntensity = 1; weights.C_Ch3_TotalIntensity = 1; weights.C_Ch1_MaxIntensity = 1; weights.C_Ch1_MeanIntensity = 1; weights.C_Ch1_MinIntensity = 1; weights.C_Ch1_WeightedCentroid = 10; weights.C_Ch2_MaxIntensity = 1; weights.C_Ch2_MeanIntensity = 1; weights.C_Ch2_MinIntensity = 1; weights.C_Ch2_WeightedCentroid = 10; weights.C_Ch3_MaxIntensity = 1; weights.C_Ch3_MeanIntensity = 1; weights.C_Ch3_MinIntensity = 1; weights.C_Ch3_WeightedCentroid = 1; weights.C_Ch1_GradientMeanIntensity = 1; weights.C_Ch1_GradientTotalIntensity = 1; weights.C_Ch2_GradientMeanIntensity = 1; weights.C_Ch2_GradientTotalIntensity = 1; weights.C_Ch3_GradientMeanIntensity = 1; weights.C_Ch3_GradientTotalIntensity = 1; weights.N_Area = 1; weights.N_BoundingBox = 1; weights.N_Centroid = 1; weights.N_Eccentricity = 1; weights.N_Solidity = 1; weights.N_MajorAxisLength = 1; weights.N_MinorAxisLength = 1; weights.N_Orientation = 1; weights.N_Ch1_TotalIntensity = 1; weights.N_Ch2_TotalIntensity = 1; weights.N_Ch3_TotalIntensity = 1; weights.N_Ch1_MaxIntensity = 1; weights.N_Ch1_MeanIntensity = 1; weights.N_Ch1_MinIntensity = 1; weights.N_Ch1_WeightedCentroid = 10; weights.N_Ch2_MaxIntensity = 1; weights.N_Ch2_MeanIntensity = 1; weights.N_Ch2_MinIntensity = 1; weights.N_Ch2_WeightedCentroid = 10; weights.N_Ch3_MaxIntensity = 1; weights.N_Ch3_MeanIntensity = 1; weights.N_Ch3_MinIntensity = 1; weights.N_Ch3_WeightedCentroid = 1; weights.N_Ch1_GradientMeanIntensity = 1; weights.N_Ch1_GradientTotalIntensity = 1; weights.N_Ch2_GradientMeanIntensity = 1; weights.N_Ch2_GradientTotalIntensity = 1; weights.N_Ch3_GradientMeanIntensity = 1; weights.N_Ch3_GradientTotalIntensity = 1; weights.N_Centroid = 1; weights.C_Centroid = 1; % weights.SaddlePoint = 0; % weights.CellSymmetry = 0; % weights.nuc_histograms = 0; % weights.NucPrctileValues = 0; % weights.CellPrctileValues = 0; % weights.CytoPrctileValues = 0;
+
+  % Works good
+  weights = {};
+  weights.N_Centroid = 1;
+  weights.C_Centroid = 1;
+
+  %% SANITY CHECK THAT METRICS EXIST IN TABLE
+  % Loop over weights, checking that they exist
+  fields = fieldnames(weights);
+  for idx = 1:length(fields)
+    metric_name = fields{idx};
+    if ~ismember(metric_name, SubsetTable.Properties.VariableNames)
+      error(['Can''t find metric in Table: ' metric_name]);
+      weights = rmfield(weights, metric_name);
+    end
+  end
+
+  %% RAW DIFFERENCES (Raw meaning within the value ranges for the metrics)
+  raw_differences = {};
+  count = 1; % this is needed because "t" time in the loop may not start at 1
+  % this way we can start processing timepoints in the middle of the sequence
   for t=min(SubsetTable.Time):max(SubsetTable.Time)-1
     fprintf('Calculating differences between frames %d and %d...\n', t, t+1)
 
     % Get only cells (ie. table rows) at T and T+1
     T1 = SubsetTable(SubsetTable.Time==t,:);
     T2 = SubsetTable(SubsetTable.Time==t+1,:);
-   
-    % Tranlation distances between T and T+1
-    X_translation = squareform(pdist([T1.Centroid(:,1);T2.Centroid(:,1)]));
-    X_translation=X_translation(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing translation
-    Y_translation = squareform(pdist([T1.Centroid(:,2);T2.Centroid(:,2)]));
-    Y_translation=Y_translation(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing translation
-    [theta,rho] = cart2pol(X_translation,Y_translation);
-    raw_differences{count}.Translation = rho;
 
-    % Eccentricity differences
-    eccentricity_diff = squareform(pdist([T1.Eccentricity;T2.Eccentricity]));
-    raw_differences{count}.Eccentricity = eccentricity_diff(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing differences
+    % Loop over weights, calculating differences for each
+    fields = fieldnames(weights);
+    for idx = 1:length(fields)
+      metric_name = fields{idx};
 
-    % Nuclear area differences
-    area_diff = squareform(pdist([T1.NArea;T2.NArea]));
-    raw_differences{count}.Area = area_diff(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing differences
+      % Handle centroids measurements differently because they contain two values, X and Y
+      if strfind(metric_name, 'Centroid')
+        % Tranlation distances between T and T+1
+        X_translation = squareform(pdist([T1.(metric_name)(:,1);T2.(metric_name)(:,1)]));
+        X_translation=X_translation(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing translation
+        Y_translation = squareform(pdist([T1.(metric_name)(:,2);T2.(metric_name)(:,2)]));
+        Y_translation=Y_translation(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing translation
+        [theta,rho] = cart2pol(X_translation,Y_translation);
+        raw_differences{count}.(metric_name) = rho;
+        continue
+      end
 
-    % MajorAxisLength differences
-    MajorAxisLength_diff = squareform(pdist([T1.MajorAxisLength;T2.MajorAxisLength]));
-    raw_differences{count}.MajorAxisLength = MajorAxisLength_diff(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing differences
-
-    % MinorAxisLength differences
-    MinorAxisLength_diff = squareform(pdist([T1.MinorAxisLength;T2.MinorAxisLength]));
-    raw_differences{count}.MinorAxisLength = MinorAxisLength_diff(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing differences
-
-    % Solidity differences
-    Solidity_diff = squareform(pdist([T1.Solidity;T2.Solidity]));
-    raw_differences{count}.Solidity = Solidity_diff(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing differences
-
-    % Orientation differences
-    Orientation_diff = squareform(pdist([T1.Orientation;T2.Orientation]));
-    raw_differences{count}.Orientation = Orientation_diff(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing differences
-
-    % Nuclear intensity differences
-    nuc_intensity_diff = squareform(pdist([T1.NInt;T2.NInt]));
-    raw_differences{count}.Nuc_intensity = nuc_intensity_diff(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing differences
-
+      % Default case, straight up difference calculation
+      difference = squareform(pdist([T1.(metric_name);T2.(metric_name)]));
+      raw_differences{count}.(metric_name) = difference(height(T1)+1:end,1:height(T1)); % produces matrix of size lenT1 x lenT2 containing differences
+    end
     count = count+1;
   end
 
-  %% NORMALIZED DIFFERENCES
+  %% NORMALIZED DIFFERENCES (Scale all metric value ranges to between 0 and 1)
   normalized_differences = {};
   for t=1:length(raw_differences)
-    normalized_differences{t}.Translation = normalize0to1(raw_differences{t}.Translation);
-    normalized_differences{t}.Eccentricity = normalize0to1(raw_differences{t}.Eccentricity);
-    normalized_differences{t}.Area = normalize0to1(raw_differences{t}.Area);
-    normalized_differences{t}.MajorAxisLength = normalize0to1(raw_differences{t}.MajorAxisLength);
-    normalized_differences{t}.MinorAxisLength = normalize0to1(raw_differences{t}.MinorAxisLength);
-    normalized_differences{t}.Solidity = normalize0to1(raw_differences{t}.Solidity);
-    normalized_differences{t}.Orientation = normalize0to1(raw_differences{t}.Orientation);
-    normalized_differences{t}.Nuc_intensity = normalize0to1(raw_differences{t}.Nuc_intensity);
+    % Loop over weights, calculating normalized differences for each
+    fields = fieldnames(weights);
+    for idx = 1:length(fields)
+      metric_name = fields{idx};
+      normalized_differences{t}.(metric_name) = normalize0to1(raw_differences{t}.(metric_name));
+    end
   end
 
-  % Importance of each metric for when calculating composite distances
-  weights = {};
-  weights.Translation = 5;
-  weights.Eccentricity = 1;
-  weights.Area = 1;
-  weights.MajorAxisLength = 1;
-  weights.MinorAxisLength = 1;
-  weights.Solidity = 1;
-  weights.Orientation = 1;
-  weights.Nuc_intensity = 2;
-
-  %% COMPOSITE DIFFERENCES
+  %% COMPOSITE DIFFERENCES (Combine differences of many metrics into one)
   composite_differences = {};
-  for t=1:length(normalized_differences)    
-    % composite_differences{t} = normalized_differences{t}.Translation .* weights.Translation;
-    composite_differences{t} = normalized_differences{t}.Translation .* weights.Translation ...
-                          + normalized_differences{t}.Eccentricity .* weights.Eccentricity ...
-                          + normalized_differences{t}.Area .* weights.Area ...
-                          + normalized_differences{t}.MajorAxisLength .* weights.MajorAxisLength ...
-                          + normalized_differences{t}.MinorAxisLength .* weights.MinorAxisLength ...
-                          + normalized_differences{t}.Solidity .* weights.Solidity ...
-                          + normalized_differences{t}.Orientation .* weights.Orientation...
-                          + normalized_differences{t}.Nuc_intensity .* weights.Nuc_intensity;
+  for t=1:length(normalized_differences)
+    composite_differences{t} = zeros(size(normalized_differences{t}.(metric_name)));
+    % Loop over weights, totalling weighted metrics
+    fields = fieldnames(weights);
+    for idx = 1:length(fields)
+      metric_name = fields{idx};
+      composite_differences{t} = composite_differences{t} + ...
+          normalized_differences{t}.(metric_name) .* weights.(metric_name);
+    end
   end
+
 end
--- tracking_pipeline.m
+++ tracking_pipeline.m
@@ -3,35 +3,42 @@ addpath(genpath('functions'))
 
 
 % Results table (Ron)
-data_path = '\\carbon.research.sickkids.ca\rkafri\OPRETTA\Operetta Processed OutPutFiles\Dataset_20171103_RB_LFS__2017_11_03T17_30_39RESULTS\ResultTable - 2 wells, 2 fields, thresh 160.mat';
+% data_path = '\\carbon.research.sickkids.ca\rkafri\OPRETTA\Operetta Processed OutPutFiles\Dataset_20171103_RB_LFS__2017_11_03T17_30_39RESULTS\ResultTable - 2 wells, 2 fields, thresh 160.mat';
 % Results table (Heather)
 % data_path = '\\carbon.research.sickkids.ca\rkafri\Heather\ResultsTables\Dataset_20170322_TG_Fibroblast_movie_2RESULTS\ResultTable.mat';
+% Results table (Camilla)
+data_path = '\\carbon.research.sickkids.ca\rkafri\DanielS\Images\20180112_CG_DPC_ROS__2018-01-12T17_35_12-Measurement1_Field41\ResultTable.mat';
 
 %% LOAD DATA
 fprintf('Loading data from "%s"...\n', data_path);
 load(data_path);
 
 % Images path
-folder = 'Z:\OPRETTA\Operetta Raw Data\Mammalian cells\20171103_RB_LFS__2017-11-03T17_30_39-Measurement1\Images\';
+% folder = 'Z:\OPRETTA\Operetta Raw Data\Mammalian cells\20171103_RB_LFS__2017-11-03T17_30_39-Measurement1\Images\'; % (Ron)
+folder = '\\carbon.research.sickkids.ca\rkafri\DanielS\Images\20180112_CG_DPC_ROS__2018-01-12T17_35_12-Measurement1_Field41\'; % (Camilla)
 
 % Select data subset (only for quicker testing/debugging)
 row = 5;
 column = 7;
-field = 19;
+field = 41;
 min_time = 1;
-max_time = 193;
+max_time = 10;
+max_time = 100;
 plate_region = sprintf('r%02dc%02df%02dp01', row, column, field); % example result: r02c04f12p01
 rows = ResultTable.Row==row & ResultTable.Column==column & ResultTable.Field==field & ResultTable.Time<=max_time & ResultTable.Time>=min_time;
 SubsetTable = ResultTable(rows,:);
 if height(SubsetTable) == 0
     error('Exiting... No cells found at the given combination of time, row, column, and field');
 end
+
 SubsetTable.TraceUsed(:,1)=0;
+
 fprintf('Operating on:\n\trow = %d\n\tcolumn = %d\n\tfield = %d\n\tmin_time = %d\n\tmax_time = %d\n',row, column, field, min_time, max_time)
+
 
 %% LOAD NUC
 fprintf('Loading NUC images...\n')
-channel = 1;
+channel = 2;
 timepoint = 1;
 filename = [folder sprintf('%s-ch%dsk%dfk1fl1.tiff', plate_region, channel, min_time)]; % load first image
 firstImg = imread(filename,1);
@@ -43,7 +50,7 @@ for i=min(SubsetTable.Time):max(SubsetTable.Time)
   nuc(:,:,count) = imread(filename,1);
   count = count+1;
 end
-%figure; imshow3D(nuc,[]);
+figure; imshow3D(nuc,[]);
 
 % %% LOAD CYTO
 fprintf('Loading CYTO images...\n')
@@ -57,7 +64,7 @@ for i=2:max(SubsetTable.Time)
   filename = [folder sprintf('%s-ch%dsk%dfk1fl1.tiff', plate_region, channel, i)];
   cyto(:,:,i) = imread(filename,1);
 end
-%figure; imshow3D(cyto,[]);
+figure; imshow3D(cyto,[]);
 
 %% CROP IMAGE AND DATASET TO BE SMALL ENOUGH TO DEBUG BY HAND
 % if exist('nuc_orig')
@@ -84,14 +91,6 @@ end
 % nuc_orig = nuc;
 % nuc = nuc_cropped;
 % figure; imshow3D(nuc,[]);
-
-% Calc x and y cell locations
-Ycoord = SubsetTable.Centroid(:,2);
-Xcoord = SubsetTable.Centroid(:,1);
-x = floor(Ycoord);
-y = floor(Xcoord);
-SubsetTable.Xcoord = x;
-SubsetTable.Ycoord = y;
     
 %% CALC DIFFERENCES BETWEEN FRAMES
 [raw_differences, normalized_differences, composite_differences] = DifferentialMeasurements(SubsetTable);
@@ -99,10 +98,11 @@ SubsetTable.Ycoord = y;
 %% TRACK CELLS
 [SubsetTable,DiffTable] = cell_tracking_v1_simple(SubsetTable, composite_differences);
 
-
 %% DEBUG
 %labelled_imgs = overlay_trace_ids_on_imgs(SubsetTable, nuc);
-coloured_imgs = overlay_nuc_and_nuc(SubsetTable, cyto);
+% coloured_imgs = overlay_nuc_and_nuc(SubsetTable, nuc);
+coloured_imgs = overlay_cyto_on_cyto(SubsetTable, cyto);
+% coloured_imgs = overlay_cyto_and_nuc_on_cyto(SubsetTable, cyto);
 colour_imgs_to_gif(coloured_imgs);
 % imgs_to_gif(labelled_imgs);
 
